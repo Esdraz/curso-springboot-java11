550cb1be9b9de23cf9babb2fbb492c6a9ac5d06e
Francisco <fsousa.jpa@gmail.com>
2022-05-16 14:49:58 -0300

    Service layer, component registration

---
+++ src/main/java/com/esdraz/webservice/services/UserService.java
@@ -0,0 +1,25 @@
+package com.esdraz.webservice.services;
+
+import com.esdraz.webservice.entities.User;
+import com.esdraz.webservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findALL() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
